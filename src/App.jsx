6fea88b70aba871178eb099be5a0199eef5dfba3
Pencho Ivanov <penchoivanov@Penchos-MacBook-Air.local>
2024-02-08 21:46:27 +0100

    1,2,3 and 4 - Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+
+import FoodBox from './components/FoodBox';
+import foodsData from './foods.json'
+import { useState } from 'react';
 import "./App.css";
 
 function App() {
+  const [food, setFood] = useState(foodsData)
+
+  const deleteFood = (Foodid) => {
+    const filterDelFood = food.filter(foods => {
+      return foods.id !== Foodid
+    })
+    setFood(filterDelFood)
+  }
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <p> Done with 1, 2, 3 and 4 go throught the videos of last night and do the rest of the LAB</p>
+      <FoodBox foodPass={food} deleteFoodPass={deleteFood}/>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,32 @@
-// Your code here
+function FoodBox({ foodPass, deleteFoodPass }) {
+    //const { food } = props
+    return(
+        <div>
+            <h1>Food Box</h1>
+
+            {foodPass.length === 0 ? <div> <p>No food avilable.</p> <p>You have delete all the food!</p> </div> : <p></p>}
+
+            {foodPass.map((x) => {
+
+                return(
+                    <div key={x.id}>
+                        <p>{x.name}</p>
+
+                        <img style={{height: "8em"}} src={x.image} />
+
+                        <p>Calories: {x.calories}</p>
+                        <p>Servings {x.servings}</p>
+
+                        <p>
+                            <b>Total Calories: {x.servings * x.calories} </b> kcal
+                        </p>
+
+                        <button onClick={() => deleteFoodPass(x.id)}>Delete</button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default FoodBox
