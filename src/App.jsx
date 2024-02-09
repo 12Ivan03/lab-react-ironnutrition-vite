660c943fc229c2d1a564230d36da4b122d1292f0
Pencho Ivanov <penchoivanov@Penchos-Air.home>
2024-02-09 14:11:37 +0100

    5-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import AddFoodForm from './components/AddFoodForm';
 import FoodBox from './components/FoodBox';
 import foodsData from './foods.json'
 import { useState } from 'react';
@@ -6,17 +7,24 @@ import "./App.css";
 
 function App() {
   const [food, setFood] = useState(foodsData)
+  //const [addFood, setAddFood] = useState(foodsData) 
+
+  const addFoodFun = (addNewFood) => {
+    const addedFood = [...food, addNewFood].reverse()
+
+    setFood(addedFood)
+  };
 
   const deleteFood = (Foodid) => {
     const filterDelFood = food.filter(foods => {
       return foods.id !== Foodid
     })
     setFood(filterDelFood)
-  }
+  };
 
   return (
     <div className="App">
-      <p> Done with 1, 2, 3 and 4 go throught the videos of last night and do the rest of the LAB</p>
+      <AddFoodForm addFoodPass={addFoodFun} />
       <FoodBox foodPass={food} deleteFoodPass={deleteFood}/>
     </div>
   );
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,83 @@
-// Your code here
+
+import { v4 as uuidv4 } from 'uuid';
+import './AddFoodForm.css'
+import { useState } from "react";
+
+
+function AddFoodForm({ addFoodPass }) {
+
+        //1.name => set and update the input names... 
+    const [name, setName] = useState('');
+    const handleName = e => setName(e.target.value);
+        //1.image 
+    const [image, setImage] = useState('');
+    const handleImage = e =>{
+        console.log('==============>',e.target)
+         return setImage(e.target.value)
+        };
+        //1.Calories
+    const [ calories, setCalories] = useState('');
+    const handleCalories = e => setCalories(e.target.value);
+        //1.Servings
+    const [ servings, setServings] = useState('');
+    const handleServings = e => setServings(e.target.value)
+
+
+
+
+        //2.handle submit form... the value of the input is taken 
+        //      from step 1. not from the input itself,
+        //      that's a difference between HTML and React...
+    const handleSubmit = (e) => {
+        e.preventDefault();  
+        //console.log('THE handleSubmit EVENT ', e);
+        const id = uuidv4();
+
+            //This variable acts as /./'Module' in Express/./ only as a concept => 
+            //  it takes the valus from the updated set... not form the input itself! 
+        const newFood = {
+            id,
+            name,
+            image,
+            calories,
+            servings
+        };
+
+            // sets the set...
+        setName('');
+        setImage('');
+        setCalories('');
+        setServings('');
+
+            //Evoke the addFoodFun through the build PROPS-'window' of 
+            //      {addFoodPass} to add the newFood.
+        addFoodPass(newFood)
+    }
+
+    return(
+        <div>
+            <h1>Create your favorite Food Box: </h1>
+            <form className="formAdd" action="" onSubmit={handleSubmit}>
+                <label> Name:
+                    <input type="text" name="name" placeholder='Salad, Fries, Drink...' value={name} onChange={handleName}/>
+                </label>
+                <br />
+                <label> Image:
+                    <input type="text" name="image" placeholder='http://...' value={image} onChange={handleImage} />
+                </label>
+                <br />
+                <label> Calories:
+                    <input type="number" name="calories" placeholder='0' value={calories} onChange={handleCalories}  />
+                </label>
+                <br />
+                <label>Servings:
+                    <input type="number" name="servings" placeholder='1' value={servings} onChange={handleServings} />
+                </label>
+                <br />
+                <button type="submit">Create</button>      
+            </form>
+        </div>
+    )
+}
+
+export default AddFoodForm
